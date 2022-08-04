1a1c6fc83f8c43a02d128335a848f8ebd2b81003
wesam-heen <107361704+wesam-heen@users.noreply.github.com>
2022-08-05 00:01:50 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let main=document.getElementById('main');
+let container=document.querySelector('.container')
+function phones(phone){
+    main.src=phone;
+}
+function background(color){
+container.style.background=color;
+}
